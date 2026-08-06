TheRodzz/vscode

Map managed VS Code policies into Agent Host managedSettings.permissions
Synthesize a client-agnostic managedSettings.permissions object from VS Code's
legacy enterprise Copilot/agent policies and forward it through the Agent Host
root config to the Copilot SDK at session create/resume.

Derive exclusively from IConfigurationService.inspect(...).policyValue, using
only rule boundaries the runtime supports:
- managed chat.tools.global.autoApprove === false -> disableBypassPermissionsMode: 'disable'
- managed chat.tools.terminal.enableAutoApprove === false -> ask: ['Shell(*)']

Per-tool eligibility (chat.tools.eligibleForAutoApproval) is intentionally not
mapped: the runtime rejects generic Tool(...) rules, so there is no supported
boundary to express it, and malformed/unknown rules reject session startup.
Network/sandbox policies are out of scope. The derived snapshot participates in
restart detection so a policy change refreshes the session before the next turn.

The published @github/copilot-sdk (^1.0.8) does not yet expose managedSettings,
so a precise additive local type mirrors the existing local-type precedent
(ICopilotRuntimeManagedSettingsSdk) until the SDK publishes the field.

Co-authored-by: Copilot App <223556219+Copilot@users.noreply.github.com>
Copilot-Session: c111c62a-eff3-4ff6-bb8a-8436a1b4babe

src/vs/platform/agentHost/node/copilot/copilotSessionLauncher.ts
@@ -11,7 +11,7 @@ import { IFileService } from '../../../files/common/files.js';
 import { ILogService, LogLevel } from '../../../log/common/log.js';
 import { CopilotCliConfigKey, applyModelFamilyAlias, copilotCliConfigSchema } from '../../common/copilotCliConfig.js';
 import { agentHostModelSupportsToolSearch, CLIENT_TOOL_SEARCH_REFERENCE_NAME } from './toolSearchDeferral.js';
-import { AgentHostSessionSyncEnabledConfigKey, platformRootSchema, type AgentHostMcpServers } from '../../common/agentHostSchema.js';
+import { AgentHostSessionSyncEnabledConfigKey, AgentHostManagedPermissionsConfigKey, platformRootSchema, type AgentHostMcpServers, type IManagedPermissions } from '../../common/agentHostSchema.js';
 import { AgentHostSandboxConfigKey, sandboxConfigSchema } from '../../common/sandboxConfigSchema.js';
 import { IAgentConfigurationService } from '../agentConfigurationService.js';
 import { IAgentHostTerminalManager } from '../agentHostTerminalManager.js';
@@ -65,9 +65,23 @@ type McpAuthResponse = Awaited<ReturnType<McpAuthHandler>>;
 type SessionHooks = NonNullable<SessionConfig['hooks']>;
 type PreToolUseHookInput = Parameters<NonNullable<SessionHooks['onPreToolUse']>>[0];
 type PostToolUseHookInput = Parameters<NonNullable<SessionHooks['onPostToolUse']>>[0];
+/**
+ * Local mirror of the SDK's `managedSettings` session-config field, scoped to
+ * the `permissions` object VS Code populates. The published
+ * `@github/copilot-sdk` types (1.0.8) expose `enableManagedSettings` but not
+ * `managedSettings`; this precise additive type lets VS Code forward
+ * enterprise-policy-derived permissions until the SDK publishes the field.
+ * Mirrors the local-type precedent used for `ICopilotRuntimeManagedSettingsSdk`
+ * in copilotAgent.ts.
+ */
+interface ICopilotManagedSettingsSdk {
+	readonly permissions?: IManagedPermissions;
+}
+
 type CopilotSessionLaunchConfig = ResumeSessionConfig & {
 	readonly pluginDirectories?: string[];
 	readonly remoteSession?: 'export';
+	readonly managedSettings?: ICopilotManagedSettingsSdk;
 };
 
 /**
@@ -83,6 +97,14 @@ export interface IActiveClientSnapshot {
 	readonly tools: readonly ToolDefinition[];
 	readonly plugins: readonly ICopilotPluginInfo[];
 	readonly mcpServers: AgentHostMcpServers;
+	/**
+	 * Enterprise-policy-derived managed permissions in effect at snapshot time.
+	 * Participates in restart detection because it is forwarded into the SDK
+	 * session config as `managedSettings.permissions`; a policy change must
+	 * refresh the session so the new permissions apply before the next turn.
+	 * Optional: `undefined` (or absent) means no managed policy applied.
+	 */
+	readonly managedPermissions?: IManagedPermissions | undefined;
 }
 
 /**
@@ -542,6 +564,7 @@ export class CopilotSessionLauncher implements ICopilotSessionLauncher {
 		// renderer reports no BYOK models), merged into the returned config so both
 		// createSession and resumeSession advertise the models to the runtime.
 		const byok = await this._resolveByokSessionConfig(plan.sessionId);
+		const managedPermissions = this._configurationService.getRootValue(platformRootSchema, AgentHostManagedPermissionsConfigKey);
 		const enableCustomTerminalTool = this._configurationService.getRootValue(copilotCliConfigSchema, CopilotCliConfigKey.EnableCustomTerminalTool) === true;
 		let shellTools: Awaited<ReturnType<typeof createShellTools>> = [];
 		if (enableCustomTerminalTool) {
@@ -633,6 +656,10 @@ export class CopilotSessionLauncher implements ICopilotSessionLauncher {
 			// events. Without this, sessions default to "off".
 			remoteSession: this._configurationService.getRootValue(platformRootSchema, AgentHostSessionSyncEnabledConfigKey) === true ? 'export' : undefined,
 			enableManagedSettings: true,
+			// Forward enterprise-policy-derived managed permissions (synthesized
+			// by VS Code from managed policy values) as the runtime's
+			// `managedSettings.permissions`. Omitted when no policy applies.
+			...(managedPermissions ? { managedSettings: { permissions: managedPermissions } } : {}),
 		};
 	}
 }

boaldecava-netizen/Ironcol

src/vs/platform/agentHost/node/copilot/copilotSessionLauncher.ts
@@ -11,7 +11,7 @@ import { IFileService } from '../../../files/common/files.js';
 import { ILogService, LogLevel } from '../../../log/common/log.js';
 import { CopilotCliConfigKey, applyModelFamilyAlias, copilotCliConfigSchema, normalizeToolSearchDeferThreshold } from '../../common/copilotCliConfig.js';
 import { agentHostModelSupportsToolSearch, CLIENT_TOOL_SEARCH_REFERENCE_NAME } from './toolSearchDeferral.js';
-import { AgentHostSessionSyncEnabledConfigKey, platformRootSchema, type AgentHostMcpServers } from '../../common/agentHostSchema.js';
+import { AgentHostManagedPermissionsConfigKey, AgentHostSessionSyncEnabledConfigKey, platformRootSchema, type AgentHostMcpServers, type IManagedPermissions } from '../../common/agentHostSchema.js';
 import { AgentSession } from '../../common/agentService.js';
 import { IAgentHostOTelService } from '../../common/otel/agentHostOTelService.js';
 import { AgentHostSandboxConfigKey, sandboxConfigSchema } from '../../common/sandboxConfigSchema.js';
@@ -83,6 +83,19 @@ type McpAuthResponse = Awaited<ReturnType<McpAuthHandler>>;
 type PreToolUseHookInput = Parameters<NonNullable<SessionHooks['onPreToolUse']>>[0];
 type PostToolUseHookInput = Parameters<NonNullable<SessionHooks['onPostToolUse']>>[0];
 /**
+ * Local mirror of the SDK's `managedSettings` session-config field, scoped to
+ * the `permissions` object VS Code populates. The published
+ * `@github/copilot-sdk` types (1.0.8) expose `enableManagedSettings` but not
+ * `managedSettings`; this precise additive type lets VS Code forward
+ * enterprise-policy-derived permissions until the SDK publishes the field.
+ * Mirrors the local-type precedent used for `ICopilotRuntimeManagedSettingsSdk`
+ * in copilotAgent.ts.
+ */
+interface ICopilotManagedSettingsSdk {
+	readonly permissions?: IManagedPermissions;
+}
+
+/**
  * Immutable snapshot of the active client's structural contributions at
  * session creation time. Used to detect when the session needs to be
  * refreshed. Root MCP servers participate in restart detection because they
@@ -95,6 +108,14 @@ export interface IActiveClientSnapshot {
 	readonly tools: readonly ToolDefinition[];
 	readonly plugins: readonly ICopilotPluginInfo[];
 	readonly mcpServers: AgentHostMcpServers;
+	/**
+	 * Enterprise-policy-derived managed permissions in effect at snapshot time.
+	 * Participates in restart detection because it is forwarded into the SDK
+	 * session config as `managedSettings.permissions`; a policy change must
+	 * refresh the session so the new permissions apply before the next turn.
+	 * Optional: `undefined` (or absent) means no managed policy applied.
+	 */
+	readonly managedPermissions?: IManagedPermissions | undefined;
 }
 
 /**
@@ -573,12 +594,13 @@ export class CopilotSessionLauncher implements ICopilotSessionLauncher {
 		}
 	}
 
-	private async _buildSessionConfig(plan: CopilotSessionLaunchPlan, runtime: ICopilotSessionRuntime): Promise<ResumeSessionConfig> {
+	private async _buildSessionConfig(plan: CopilotSessionLaunchPlan, runtime: ICopilotSessionRuntime): Promise<ResumeSessionConfig & { managedSettings?: ICopilotManagedSettingsSdk }> {
 		const plugins = plan.snapshot.plugins;
 		// Synthesize BYOK provider/model config (empty when BYOK is gated off or the
 		// renderer reports no BYOK models), merged into the returned config so both
 		// createSession and resumeSession advertise the models to the runtime.
 		const byok = await this._resolveByokSessionConfig(plan.sessionId);
+		const managedPermissions = this._configurationService.getRootValue(platformRootSchema, AgentHostManagedPermissionsConfigKey);
 		const enableCustomTerminalTool = this._configurationService.getRootValue(copilotCliConfigSchema, CopilotCliConfigKey.EnableCustomTerminalTool) === true;
 		let shellTools: Awaited<ReturnType<typeof createShellTools>> = [];
 		if (enableCustomTerminalTool) {
@@ -677,6 +699,10 @@ export class CopilotSessionLauncher implements ICopilotSessionLauncher {
 			// events. Without this, sessions default to "off".
 			remoteSession: this._configurationService.getRootValue(platformRootSchema, AgentHostSessionSyncEnabledConfigKey) === true ? 'export' : undefined,
 			enableManagedSettings: true,
+			// Forward enterprise-policy-derived managed permissions (synthesized
+			// by VS Code from managed policy values) as the runtime's
+			// `managedSettings.permissions`. Omitted when no policy applies.
+			...(managedPermissions ? { managedSettings: { permissions: managedPermissions } } : {}),
 		};
 	}
 }